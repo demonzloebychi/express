2174baa366a2526276bc698d7f51aee25478e856
nikolas <nikolaislnkv@gmail.com>
2025-05-15 12:59:11 +0300

    upd css header and upd popu, add footer

--- public/js/popup.js
+++ public/js/popup.js
@@ -1,3 +1,43 @@
+ // Получаем все элементы с классом 'phones'
+  const phoneInputs = document.querySelectorAll('.input__phone');
+
+  // Для каждого элемента добавляем обработчик события keydown
+  phoneInputs.forEach(phoneInput => {
+    phoneInput.addEventListener('keydown', function(e) {
+      inputphone(e, this); // Используем 'this' для передачи текущего элемента
+    });
+  });
+
+  // Функция маски для телефона
+  function inputphone(e, phone) {
+    function stop(evt) {
+      evt.preventDefault();
+    }
+
+    let key = e.key,
+      v = phone.value;
+    not = key.replace(/([0-9])/, 1);
+
+    if (not == 1 || 'Backspace' === not) {
+      if ('Backspace' != not) {
+        if (v.length < 3 || v === '') {
+          phone.value = '+7 (';
+        }
+        if (v.length === 7) {
+          phone.value = v + ') ';
+        }
+        if (v.length === 12) {
+          phone.value = v + '-';
+        }
+        if (v.length === 15) {
+          phone.value = v + '-';
+        }
+      }
+    } else {
+      stop(e);
+    }
+  }
+
 const popup = document.querySelector('#popup_regForm')
 const popupClose = document.querySelector('.popup__close')
 const submitForm = document.querySelector('#submit')
@@ -48,30 +88,45 @@ const numberInput = document.querySelector('#number')
 
 
 const popupCall = document.querySelector('#popup-call');
-const popupCallBtn = document.querySelector('#popup-call-btn');
+const popupCallBtn = document.querySelectorAll('.popup-call-btn');
 const popupCallSubmit = document.querySelector('#popup-call__submit')
+const popupCallOverlay = document.querySelector('.popup-call__overlay')
 
+const popupCallPhone = document.querySelectorAll('.popup-call__phone')
 
-popupCallBtn.addEventListener('click', function() {
-    popupCall.classList.toggle('active');
-});
+popupCallBtn.forEach(btn => {
+    btn.addEventListener('click', function() {
+        popupCall.classList.toggle('active');
+        popupCallOverlay.classList.toggle('active');
+
+    });
+})
+
+ 
 
 popupCallSubmit.addEventListener('click', function(e) {
     e.preventDefault(); 
     // highlightEmptyFields();
-
-    if (checkFields()) {
-        popupCall.classList.remove('active')
+    popupCall.classList.remove('active')
+        popupCallOverlay.classList.remove('active')
         popup.classList.add('active')
         popupOverlay.classList.add('active')
-    } else {
-        alert('Пожалуйста, заполните Обязательные поля!');
-    }
+
+
+    // if (checkFields()) {
+    //     popupCall.classList.remove('active')
+    //     popupCallOverlay.classList.remove('active')
+    //     popup.classList.add('active')
+    //     popupOverlay.classList.add('active')
+    // } else {
+    //     alert('Пожалуйста, заполните Обязательные поля!');
+    // }
 })
 
 
 const closePopupCall = () => {
     popupCall.classList.remove('active')
+    popupCallOverlay.classList.remove('active')
 }
 popupClose.addEventListener('click', closePopupCall)
 
