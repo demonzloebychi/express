fec7edd91ecda4c1f2587d575b60014d16f3a61a
nikolai <adflame1342@gmail.com>
2025-06-30 15:38:27 +0300

    upd

--- public/js/case-single.js
+++ public/js/case-single.js
@@ -45,6 +45,34 @@ const otherCaseSwiper = new Swiper('.other-case-swiper', {
       spaceBetween: 16,   // или другой нужный отступ
     }
   }
+});
 
 
+const moreCaseSwiper = new Swiper('.more-case-swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  // loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.more-case-swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.more-case-swiper-button-next',
+    prevEl: '.more-case-swiper-button-prev',
+  },
+   spaceBetween: 20,
+
+   slidesPerView: 1, // по умолчанию на десктопе
+  breakpoints: {
+    992: { // на планшетах и выше
+      slidesPerView: 2,
+    },
+    0: { // на мобильных
+      slidesPerView: 1.1, // или 1.15 — подберите под свой дизайн
+      spaceBetween: 16,   // или другой нужный отступ
+    }
+  }
 });
