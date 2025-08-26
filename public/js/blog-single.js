6469feba185bb2d539f813ac55787772badd108c
nikolai <adflame1342@gmail.com>
2025-08-26 16:34:22 +0300

    upd blog

--- public/js/blog-single.js
+++ public/js/blog-single.js
@@ -1,68 +1,71 @@
 const reactionButtons = document.querySelectorAll(".a-blog-reaction__button");
 
-const pathSegments = window.location.pathname.split('/')
-const articleSlug = pathSegments[pathSegments.length -1] || pathSegments[pathSegments.length - 2];
+const pathSegments = window.location.pathname.split("/");
+const articleSlug =
+  pathSegments[pathSegments.length - 1] ||
+  pathSegments[pathSegments.length - 2];
 
-const reactionKey = `reaction_for_${articleSlug}`
+const reactionKey = `reaction_for_${articleSlug}`;
+
+const reactionPopup = document.querySelector(".a-blog-reaction-popup");
+const reactionPopupClose = document.querySelector(
+  ".a-blog-reaction-popup__close"
+);
 
 reactionButtons.forEach((btn, index) => {
   btn.addEventListener("click", function () {
-    reactionButtons.forEach(b => b.classList.remove('active'));
-    btn.classList.add('active');
-
+    reactionButtons.forEach((b) => b.classList.remove("active"));
+    btn.classList.add("active");
     localStorage.setItem(reactionKey, index);
+    reactionPopup.classList.add("active");
   });
 });
 
-window.addEventListener('load', () => {
-  const savedIndex = localStorage.getItem(reactionKey); 
+reactionPopupClose.addEventListener("click", function () {
+  reactionPopup.classList.remove("active");
+});
+
+window.addEventListener("load", () => {
+  const savedIndex = localStorage.getItem(reactionKey);
   if (savedIndex !== null) {
     reactionButtons.forEach((btn, index) => {
-      btn.classList.toggle('active', index == savedIndex); 
+      btn.classList.toggle("active", index == savedIndex);
     });
   }
 });
 
-
-
-document.querySelectorAll('.a-blog-navigation__list a').forEach(link => {
-  link.addEventListener('click', event => {
+document.querySelectorAll(".a-blog-navigation__list a").forEach((link) => {
+  link.addEventListener("click", (event) => {
     event.preventDefault();
     const targetText = link.textContent.trim();
-    const heading = Array.from(document.querySelectorAll('h2, h3, h4, h5, h6'))
-      .find(h => h.textContent.trim() === targetText);
+    const heading = Array.from(
+      document.querySelectorAll("h2, h3, h4, h5, h6")
+    ).find((h) => h.textContent.trim() === targetText);
     if (heading) {
-      heading.scrollIntoView({ behavior: 'smooth' });
+      heading.scrollIntoView({ behavior: "smooth" });
     }
   });
 });
 
+const openArticleNavBtn = document.querySelector(".js-open-article-nav");
+const articleNavMobile = document.querySelector(
+  ".a-blog-single-article__nav-mobile"
+);
 
+openArticleNavBtn.addEventListener("click", function () {
+  articleNavMobile.classList.toggle("active");
+});
 
-
-const openArticleNavBtn = document.querySelector('.js-open-article-nav')
-const articleNavMobile = document.querySelector('.a-blog-single-article__nav-mobile')
-
-openArticleNavBtn.addEventListener('click', function() {
-  articleNavMobile.classList.toggle('active')
-
-
-
- 
-})
-
-window.addEventListener('resize', function() {
+window.addEventListener("resize", function () {
   if (window.innerWidth > 1024) {
-    articleNavMobile.classList.remove('active')
+    articleNavMobile.classList.remove("active");
   }
-})
-  
+});
 
-
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   // Optional parameters
   loop: true,
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   breakpoints: {
     320: {
       // slidesPerView: 1,
@@ -75,18 +78,16 @@ const swiper = new Swiper('.swiper', {
     1440: {
       // slidesPerView: 3,
       spaceBetween: 40,
-    }
+    },
   },
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-
-
 });
